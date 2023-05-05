d274003a8bc9a11356eeebbfb0eec65f92b13e5b
Bravo1B9 <brofe93@gmail.com>
2023-05-05 14:59:59 +0000

    Initial todos module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [TodosModule],
   controllers: [],
   providers: [],
 })
